cfc381645b47467c73a2e52137d37f8b29139a58
Pratima Gaihre <97714023+pratiga@users.noreply.github.com>
2023-07-21 02:35:07 -0700

    routing for multiple page
    add routing

--- online_store/src/App.js
+++ online_store/src/App.js
@@ -2,13 +2,31 @@
 
 import Header from "./Layout/Header";
 import Item from "./Product/Item";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import ItemDetails from "./Product/ItemDetails";
 
 function App() {
+
+  
   return (
+
+    <BrowserRouter>
     <div className="App">
-     <Header />
-     <Item />
+      <Header />
     </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/item" element={<Item />} />
+      {/* <Route path="/service" element={<Service />} /> */}
+      
+      <Route path="/itemDetails/:id" element={<ItemDetails />}></Route>
+     
+    </Routes>
+  </BrowserRouter>
+
+
+
   );
 }
 
